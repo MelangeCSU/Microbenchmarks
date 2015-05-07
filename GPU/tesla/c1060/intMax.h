9fb00d45ae679dfba1d23f0d7415fb3a5d08f971
revraj <revathy.rajasree@gmail.com>
2015-05-07 16:41:27 -0600

    Adding files for integer Max-plus benchmark with all data in registers - for GPU tesla c1060 arch

---
+++ GPU/tesla/c1060/intMax.h
@@ -0,0 +1,17 @@
+///
+/// intMax.h
+/// For CSU CS575 Spring 2011
+/// Instructor: Wim Bohm
+/// Based on code from the CUDA Programming Guide
+/// By David Newman
+/// Created: 2011-02-16
+/// Last Modified: 2011-02-16 DVN
+///
+/// Kernels written for use with this header
+/// add two Vectors A and B in C on GPU
+/// 
+
+
+__global__ void CompareAddVectors(const float* A, const float* B, float* C, int x, int k);
+
+__global__ void CompareAddVectors(const int* A, const int* B, int* C, int x, int k);
