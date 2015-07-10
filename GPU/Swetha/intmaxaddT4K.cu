c4094a908283b9c4d79d3b1d304c1fd90487893b
Swetha Varadarajan <swesri@lion.cs.colostate.edu>
2015-07-10 09:43:27 -0600

    modified

--- GPU/Swetha/intmaxaddT4K.cu
+++ GPU/Swetha/intmaxaddT4K.cu
@@ -3,7 +3,7 @@ __global__ void IntmaxaddT4(int* C, int k, int sl, int sharedsize)
 
 
 	int i,t;
-	int a=0,a2=0,b=02;
+	int a=0,a2=0,b=0;
 	int blockStartIndex  = blockIdx.x * blockDim.x * sharedsize;
 	int threadStartIndex = blockStartIndex + threadIdx.x ;
 	__shared__ int c[4000];
@@ -14,10 +14,13 @@ __global__ void IntmaxaddT4(int* C, int k, int sl, int sharedsize)
 		c[threadIdx.x+i]=C[threadIdx.x+i];
 
 	for (t = 0; t < k; t++) {
-		a=max(a,c[threadIdx.x]+10);
+		
 		#pragma unroll
-		for (i = threadStartIndex+sl; i < sharedsize; i+=sl){
-		a2=c[i];c[i] = max(a,(c[i]+b));a=a2;
+		for (i = threadStartIndex; i < sharedsize; i+=sl){
+			a2=c[i];
+			c[i] = max(a,(c[i]+b));
+			a=a2;
+		a=max(a,c[threadIdx.x]+10);
 	}}
 
 	#pragma unroll
